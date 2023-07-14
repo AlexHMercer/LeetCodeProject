daf90e5b2d8d87ed8d800cd28be554671bb7ceb2
AlexMercer <745164733@qq.com>
2023-07-14 18:38:42 +0800

    补充题目

--- src/com/leetcode/easy/FindRepeatNumber.java
+++ src/com/leetcode/easy/FindRepeatNumber.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+ * 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+ * 请找出数组中任意一个重复的数字。
+ *
+ */
+
 public class FindRepeatNumber {
 
     public int findRepeatNumber(int[] nums) {
