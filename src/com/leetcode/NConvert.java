458c9eacbb0b2db7b9d73d822de2f7b83ecc6de8
AlexMercer <745164733@qq.com>
2023-07-18 17:10:16 +0800

    N字形变换，题目描述

--- src/com/leetcode/NConvert.java
+++ src/com/leetcode/NConvert.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 N 字形排列。
  */
 
 public class NConvert {
