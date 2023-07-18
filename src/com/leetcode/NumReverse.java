cfb987b359f003bd0f298279b438cf37cd874bb7
AlexMercer <745164733@qq.com>
2023-07-18 17:04:11 +0800

    含符号的数字反转

---
+++ src/com/leetcode/NumReverse.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+public class NumReverse {
+    public int reverse(int x) {
+        String xString = Integer.toString(x);
+        String string = xString;
+        int flag = 1;
+        if (x < 0) {
+            flag = -1;
+            string = xString.substring(1);
+        }
+        try {
+            return Integer.valueOf((new StringBuilder(string)).reverse().toString()) * flag;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+}
