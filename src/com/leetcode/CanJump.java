53e8a370481c88d50fd5308b4574c3489a351aa9
AlexMercer <745164733@qq.com>
2023-07-30 17:35:35 +0800

    跳跃游戏，Q1：能否到达最后；Q2：到达最后的最少次数，Q2F1：从后往前O(n^2)；Q2F2：从前往后O(n)

--- src/com/leetcode/CanJump.java
+++ src/com/leetcode/CanJump.java
@@ -7,6 +7,12 @@ import java.util.LinkedList;
  * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
  * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
  * 判断你是否能够到达最后一个下标。
+ * 问题2：给出到达最后一个下标的最小次数
+ * 法1：从后往前，O(n^2)
+ * 法2：从前往后，O(n)
+ *      从前往后就是：每一个位置都可以确定可到达的范围，在这个范围中又可以根据值确定最大的下一个可到达范围，
+ *      这样就能通过一次遍历找到最小次数。先标记end和最大可到达范围，遍历中，最大可到达范围根据值的不同而不同，
+ *      在遍历完end之前的值之后，将end调整为最大可到达范围，当end>=最右边的index时，最小次数就得到了。
  *
  * 分析：
  *      题目要求判断能否根据数组中的步数到达最后一个下标，而能否到达此下标与上一个所在的位置有关，
@@ -37,11 +43,47 @@ public class CanJump {
         }
         return false;
     }
+    public int jump(int[] nums) {
+        if (nums.length == 1){
+            return 1;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            int minStep = dp.length;
+            int minIndex = i;
+            for (int j = 0; j < i; j++) {
+                if (i-j<=nums[j] && dp[j]<minStep){
+                    minIndex = j;
+                    minStep = dp[j];
+                }
+            }
+            dp[i] = dp[minIndex]+1;
+        }
+        return dp[dp.length-1];
+    }
+    public int jump2(int[] nums) {
+        if (nums.length == 1){
+            return 0;
+        }
+        int end = nums[0];
+        int maxPosition = end;
+        int ans = 1;
+        for (int i = 1; i < nums.length-1; i++) {
+            maxPosition = Math.max(maxPosition,i+nums[i]);
+            if (i==end){
+                ans++;
+                end = maxPosition;
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         CanJump canJump = new CanJump();
         boolean b = canJump.canJump(new int[]{2,3,1,1,4});
-        System.out.println(b);
-
+        //System.out.println(b);
+        int jump = canJump.jump2(new int[]{1, 1, 1, 1, 4});
+        System.out.println(jump);
     }
 }
